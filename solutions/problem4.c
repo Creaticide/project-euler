078f66f0c3b0bc8f1c4186da88904fda43b0a299
Henri Aho <henri.aho@outlook.com>
2018-10-02 22:45:28 +0300

    Problem 4.

---
+++ solutions/problem4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Objective: "Find the largest palindrome made from the product of two 3-digit numbers."
+ */
+
+int is_palindrome(int n);
+
+void problem_4()
+{
+	int a, b, c, max = 0;
+
+	for (a = 100; a < 1000; a++) {
+		for (b = 100; b < 1000; b++) {
+			c = a * b;
+			if (is_palindrome(c) == 1 && c > max) {
+				max = c;
+			}
+		}
+	}
+
+	printf("Problem 4: %d\n", max);
+}
+
+int is_palindrome(int n)
+{
+	char str[6];
+	int len;
+
+	sprintf(str, "%d", n);
+	len = strlen(str);
+
+	for (int i = 0; i < len / 2; i++) {
+		if (str[i] != str[len - i - 1]) {
+			return 0;
+		}
+	}
+
+	return 1;
+}
--- solutions/problems.h
+++ solutions/problems.h
@@ -4,5 +4,6 @@
 void problem_1();
 void problem_2();
 void problem_3();
+void problem_4();
 
 #endif /* SOLUTIONS_PROBLEMS_H_ */
